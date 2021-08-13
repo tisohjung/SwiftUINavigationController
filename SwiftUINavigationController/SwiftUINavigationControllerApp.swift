86527753fda31e38027737e0a0f898dec7cf99c4
Minho Yi <minho.yi@riiid.co>
2021-08-13 10:26:09 +0900

    init Project

---
+++ SwiftUINavigationController/SwiftUINavigationControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationControllerApp.swift
+//  SwiftUINavigationController
+//
+//  Created by minho on 2021/08/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
